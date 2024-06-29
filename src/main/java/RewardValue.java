475e6ca530327768106cfccbd1a1ee02a2bea7c7
Durga <durgasadasivuni23@gmail.com>
2024-06-29 21:41:51 +0530

    updated tests

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,39 +1,28 @@
-
 public class RewardValue {
-    private static final double MILES_TO_CASH_RATE = 0.0035;
-    private double cash;
-    private double miles;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-    // Constructor for cash value
-    public RewardValue(double cash) {
-        this.cash = cash;
-        this.miles = cash / MILES_TO_CASH_RATE;
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
 
-    // Constructor for miles value
-    public RewardValue(double miles, boolean isMiles) {
-        if (isMiles) {
-            this.miles = miles;
-            this.cash = miles * MILES_TO_CASH_RATE;
-        }
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
 
-    // Method to get cash value
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
     public double getCashValue() {
-        return cash;
+        return cashValue;
     }
 
-    // Method to get miles value
-    public double getMilesValue() {
-        return miles;
-    }
-
-    public static void main(String[] args) {
-        RewardValue rv1 = new RewardValue(100); // cash constructor
-        RewardValue rv2 = new RewardValue(10000, true); // miles constructor
-
-        System.out.println("RV1 Cash: " + rv1.getCashValue() + ", Miles: " + rv1.getMilesValue());
-        System.out.println("RV2 Cash: " + rv2.getCashValue() + ", Miles: " + rv2.getMilesValue());
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
 }
-
